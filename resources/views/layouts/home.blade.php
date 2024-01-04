b7f33ec503f5f7f764c625be97a27d569bd0dbcf
vencesla <admirmabiki@gmail.com>
2024-01-04 10:22:49 +0100

    Adding laravel project

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,8 @@
+@extends('app')
+
+@section('content')
+<div class="container">
+    <h1 class="text-center mt-4">Bienvenu sur mon site en Laravel 10</h1>
+    <a href="#" class="btn btn-primary">Inscriptiion</a>
+</div>
+@stop
